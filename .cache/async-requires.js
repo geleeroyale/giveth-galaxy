2df1861eb1d46e18f7b42adb3e0228aaad8da183
geleeroyale <krassvs@gmail.com>
2018-10-10 18:17:44 +0200

    update .gitignore

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,7 +2,7 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/home/rogerwilco/github/giveth-galaxy/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": () => import("/home/rogerwilco/github/giveth-galaxy/node_modules/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---node-modules-gatsby-plugin-offline-app-shell-js" */),
   "component---src-pages-404-js": () => import("/home/rogerwilco/github/giveth-galaxy/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-index-js": () => import("/home/rogerwilco/github/giveth-galaxy/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-page-2-js": () => import("/home/rogerwilco/github/giveth-galaxy/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -3,7 +3,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/home/rogerwilco/github/giveth-galaxy/.cache/dev-404-page.js")),
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": preferDefault(require("/home/rogerwilco/github/giveth-galaxy/node_modules/gatsby-plugin-offline/app-shell.js")),
   "component---src-pages-404-js": preferDefault(require("/home/rogerwilco/github/giveth-galaxy/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/home/rogerwilco/github/giveth-galaxy/src/pages/index.js")),
   "component---src-pages-page-2-js": preferDefault(require("/home/rogerwilco/github/giveth-galaxy/src/pages/page-2.js"))
