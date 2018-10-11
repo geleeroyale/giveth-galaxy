4d31b799cf672bd18952afbbc3df54251be86cfe
geleeroyale <krassvs@gmail.com>
2018-10-11 04:00:56 +0200

    remove unnecessary files

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/home/rogerwilco/github/giveth-galaxy/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/home/rogerwilco/github/giveth-galaxy/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
