9ecabb4a28385ead6ccc89765ca9c0899af697e7
geleeroyale <krassvs@gmail.com>
2018-10-11 05:32:10 +0200

    added gatsby-styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,44 @@
 module.exports = {
   siteMetadata: {
-    title: 'Giveth',
-    description: 'Building the future of Giving',
+    title: "Giveth",
+    description: "Building the future of Giving"
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#000',
-        theme_color: '#422453',
-        display: 'minimal-ui',
-        icon: 'src/images/giveth-favicon.png', // This path is relative to the root of the site.
-      },
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#000",
+        theme_color: "#422453",
+        display: "minimal-ui",
+        icon: "src/images/giveth-favicon.png" // This path is relative to the root of the site.
+      }
     },
-    'gatsby-plugin-offline',
+    "gatsby-plugin-offline",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: `${__dirname}/src/data/`,
-      },
+        path: `${__dirname}/src/data/`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `img`,
-        path: `${__dirname}/src/images/`,
-      },
+        path: `${__dirname}/src/images/`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      }
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-  ],
-}
+    `gatsby-plugin-sharp`
+  ]
+};
