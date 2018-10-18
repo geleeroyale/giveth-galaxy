75380aaf8855cdebb91ffb1f0a5310734d36dfce
geleeroyale <krassvs@gmail.com>
2018-10-18 14:43:57 +0200

    removed unused plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,22 +52,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images-contentful`,
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 590
-            }
-          }
-        ]
-      }
-    },
     `gatsby-plugin-netlify` // make sure to put last in the array
   ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { graphql } from "gatsby";
 
 import HeroBackground from "../images/m82.jpg";
 
