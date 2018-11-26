ba35e13ca794058434837595d3ec819d2347cfd4
geleeroyale <krassvs@gmail.com>
2018-11-26 18:42:51 +0100

    mobile

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,14 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Helmet from "react-helmet";
-import { StaticQuery, graphql } from "gatsby";
-import { createGlobalStyle } from "styled-components";
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from 'gatsby'
+import { createGlobalStyle } from 'styled-components'
 
-import { TypographyStyle, GoogleFont } from "react-typography";
-import typography from "../utils/typography";
+import { TypographyStyle, GoogleFont } from 'react-typography'
+import typography from '../utils/typography'
 
-import Header from "./header";
-import Footer from "./footer";
+import Header from './header'
+import Footer from './footer'
 
 const GlobalStyle = createGlobalStyle`
   html {
@@ -35,13 +35,7 @@ h2 {
   font-size: 2rem; 
   font-weight: 300;
 }
-
-@media (max-width: 640px) {
-  body {
-    padding: 0 1rem;
-  }  
-}
-`;
+`
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -62,13 +56,13 @@ const Layout = ({ children }) => (
           title={data.site.siteMetadata.title}
           meta={[
             {
-              name: "description",
-              content: data.site.siteMetadata.description
+              name: 'description',
+              content: data.site.siteMetadata.description,
             },
             {
-              name: "keywords",
-              content: data.site.siteMetadata.keywords
-            }
+              name: 'keywords',
+              content: data.site.siteMetadata.keywords,
+            },
           ]}
         >
           <html lang="en" />
@@ -79,10 +73,10 @@ const Layout = ({ children }) => (
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
-            margin: "0 auto",
+            margin: '0 auto',
             // maxWidth: 960,
             // padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0
+            paddingTop: 0,
           }}
         >
           {children}
@@ -91,10 +85,10 @@ const Layout = ({ children }) => (
       </>
     )}
   />
-);
+)
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired
-};
+  children: PropTypes.node.isRequired,
+}
 
-export default Layout;
+export default Layout
