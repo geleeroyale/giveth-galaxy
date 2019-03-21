e29ab0c4fff716018a37e5974bae1eba3bc7aa72
geleeroyale <krassvs@gmail.com>
2019-03-21 15:00:52 +0100

    add google analytics, fixes #4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,66 +1,72 @@
-require("dotenv").config();
+require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: "Giveth",
+    title: 'Giveth',
     description:
-      "A community of developers - Building the future of Giving - #blockchainforgood",
-    keywords: "donation, ethereum, blockchain, crypto, dapp, donate, eth",
-    siteUrl: `https://www.giveth.io`
+      'A community of developers - Building the future of Giving - #blockchainforgood',
+    keywords: 'donation, ethereum, blockchain, crypto, dapp, donate, eth',
+    siteUrl: `https://www.giveth.io`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
-        start_url: "/",
-        background_color: "#000",
-        theme_color: "#422453",
-        display: "minimal-ui",
-        icon: "src/images/giveth-favicon.png" // This path is relative to the root of the site.
-      }
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#000',
+        theme_color: '#422453',
+        display: 'minimal-ui',
+        icon: 'src/images/giveth-favicon.png', // This path is relative to the root of the site.
+      },
     },
     `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID || "",
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || ""
-      }
+        spaceId: process.env.CONTENTFUL_SPACE_ID || '',
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || '',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GA || '',
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `img`,
-        path: `${__dirname}/src/images/`
-      }
+        path: `${__dirname}/src/images/`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: `${__dirname}/src/data/`
-      }
+        path: `${__dirname}/src/data/`,
+      },
     },
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         // Add any options here
-      }
+      },
     },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography`
-      }
+        pathToConfigModule: `src/utils/typography`,
+      },
     },
     `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-remark`,
     `gatsby-plugin-sitemap`,
-    `gatsby-plugin-netlify` // make sure to put last in the array
-  ]
-};
+    `gatsby-plugin-netlify`, // make sure to put last in the array
+  ],
+}
